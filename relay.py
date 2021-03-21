159d92841174b8bdafb13abf4de6f44cd6b2e62e
barkingblues <nicksrk@gmail.com>
2021-03-21 19:07:29 +0530

    Turning everything off by default

--- relay.py
+++ relay.py
@@ -1,15 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 import json
+from artificialLighting import artificialLight
 
 #%% Switch function definitions
 def turn_on(pin, deviceName = ''):
     GPIO.output(pin, GPIO.LOW)
-    print(deviceName+'Turned On')
+    print(deviceName + 'Turned On')
 
 def turn_off(pin, deviceName = ''):
     GPIO.output(pin, GPIO.HIGH)
-    print(deviceName+'Turned Off')
+    print(deviceName + 'Turned Off')
 
 #%% Load config file
 
@@ -18,12 +19,13 @@ relayConfig = json.load(relayConfigFile)
 
 print('Relay Configurations Loaded')
 
-#%% Initialization
+#%% Relay Initialization
 
 GPIO.setmode(GPIO.BCM)
 
 for pin in relayConfig['GPIOPins']:
 	GPIO.setup(pin, GPIO.OUT)
+    turn_off(pin)
 
 time.sleep(1)
 print('GPIOs Initiated.')
@@ -44,3 +46,8 @@ except KeyboardInterrupt:
     turn_off([GPIOPins[relayNumber - 1]])
     time.sleep(1)
     GPIO.cleanup()
+
+#%% Light
+
+#lightRelayPin = 2
+#lightObject = artificialLight(GPIOPins[lightRelayPin - 1], "06:00", "18:00", "LED Lamp")
--- testing.py
+++ testing.py
@@ -1,20 +1,19 @@
-import json
+import relay
+import schedule
 
-x = {
-    1: {
-        'GPIOPin': 27,
-        'name': 'Water Pump'
-    },
-    2: {
-        'GPIOPin': 28,
-        'name': ''
-    },
-    3: {
-        'GPIOPin': 28,
-        'name': ''
-    },
-    4: {
-        'GPIOPin': 28,
-        'name': ''
-    }
-}
+class artificialLight:
+    def __init__(self, GPIOPin, startTime, stopTime):
+        self.GPIOPin = GPIOPin
+        self.startTime = startTime
+        self.stopTime = stopTime
+        self.runSchedule()
+
+    def runSchedule(self):
+        schedule.every().day.do(self.turnOn)
+        schedule.every().day.do(self.turnOff)
+
+    def turnOn(self):
+        relay.turn_on(self.GPIOPin)
+
+    def turnOff(self):
+        relay.turn_off(self.GPIOPin)
